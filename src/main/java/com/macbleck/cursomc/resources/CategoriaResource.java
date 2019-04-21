f808e47bfb8e1e6b557a36241df6dac71bdcf92b
mac <mauricio.bleck7@gmail.com>
2019-04-21 02:41:20 -0300

    Testando a primeira classe de dominio - categoria

--- src/main/java/com/macbleck/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/macbleck/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.macbleck.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.macbleck.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,16 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+				
+		return lista;
 	}
 
 }
